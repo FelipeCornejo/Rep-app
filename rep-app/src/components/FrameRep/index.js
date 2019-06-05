bcfb9a8d037c2723054f98b1c259cbc72328cde7
Felipe Cornejo <49447598+FelipeCornejo@users.noreply.github.com>
2019-06-05 13:52:59 -0400

    Update index.js

--- rep-app/src/components/FrameRep/index.js
+++ rep-app/src/components/FrameRep/index.js
@@ -3,19 +3,21 @@ import Usuario from './Usuario';
 import Repositorios from './Repositorios';
 import './Styles.css';
 
+const username = "alanrubin";
 const BASE_URL = "https://api.github.com";
-
+const url = `${BASE_URL}/users/${username}/repos?per_page=250`;
 
 const data =  {
-    nombre: "nombre de la wea",
-    descripcion: "asd",
-    fecha: "/ /"
+    nombre: "nombre del repositorio",
+    lenguaje: "Ruby",
+    descripcion: "descripcion"
 }
 
-const data2 =  {
-    nombre: "nombre de la wea1!!!!",
-    descripcion: "asd!!!!!",
-    fecha: "/ /!!!!!"
+//creare un data2 para que se visualice la actualizacion de la funcion handleUpdateClick
+const data2 = {
+    nombre: "nombre de repositorio2",
+    lenguaje: "Js",
+    descripcion: "descripcion2"
 }
 
 class FrameRep extends Component {
@@ -23,19 +25,37 @@ class FrameRep extends Component {
     constructor() {
         super();
         this.state = {
-            username: 'littlecornejo',
+            username: 'alanrubin',
             data: data,
-        }
+        };
     }
+//lo siguiente no se pudo resolver (getData) ya que no supe como pedir la informacion dentro de la api.
+//ya que era una array de arrays, no encontre el camino adecuado para llegar a pedir la info.
+    //getData = url => {
+        //const { language } = url.language;
+        //const { description } = url.description;
+        //const { name } = url.name;
 
+        //const data = {
+            //lenguaje : language ,
+            //descripcion : description,
+            //nombre : name,
+        //};
+
+        //return data;
+    //}
+    
+//Actualizacion de la pagina para cuando cambie de usuario, por mientras hardcoreado con un boton de "buscar" para ver si cambia la informacion
+//interna (dentro de FrameRep)
+//hardocreado con data2 para previsualizar los cambios
+//la idea es poner un fetch que resuelva la constante url y me entregue la info que pido en getData
     handleUpdateClick = () => {
-        fetch(url);
+        fetch(url)
 
-        console.log("actualizado;");
+        console.log ("actualizado");
 
         this.setState({
-            username: 'alanrubin',
-            data: data2,
+            data: data2
         });
     }
 
